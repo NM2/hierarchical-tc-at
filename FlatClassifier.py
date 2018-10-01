496bad37aec263105e9b73b31292f0b959732b38
antmontieri <43752765+antmontieri@users.noreply.github.com>
2018-10-01 16:57:26 +0200

    Added licenses to python codes

--- FlatClassifier.py
+++ FlatClassifier.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python2
 
+#    Implements flat classifiers employed in the paper "A Dive into the Dark Web: Hierarchical Traffic Classification of Anonymity Tools".
+#
+#    Copyright (C) 2018  Giampaolo Bovenzi & Antonio Montieri
+#    email: traffic@unina.it, giampaolo.bovenzi@gmail.com, antonio.montieri@unina.it
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import arff
 import numpy as np
 import copy
